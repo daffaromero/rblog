e8fc3d614b91fb155f0c79b724980efd42c93d0a
Daffa Romero <daffaromero@gmail.com>
2025-07-06 19:33:20 +0700

    devise auth setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Post.create([
+  { title: "First Post", body: "This is the content of the first post." },
+  { title: "Second Post", body: "This is the content of the second post." },
+  { title: "Third Post", body: "This is the content of the third post." }
+])
